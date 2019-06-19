c96d7ebc44eca152c674f1c2477c13057e5878db
ferdi <ferdireza96@gmail.com>
2019-06-19 11:12:39 +0700

    fix exit with back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
   constructor(
@@ -21,23 +21,16 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      
       // let status bar overlay webview
       this.statusBar.overlaysWebView(false);
       // set status bar to white
-      this.statusBar.backgroundColorByHexString('#2e7c5a');
+      this.statusBar.backgroundColorByHexString("#2e7c5a");
 
       this.splashScreen.hide();
     });
 
-    this.platform.backButton.subscribe(()=>{
-      if (this.router.url === "/home/mtk") {
-        navigator['app'].exitApp();
-      }
-
-      if (this.router.url === "/home/ipa" || this.router.url === "/home/theinfo") {
-        this.router.navigateByUrl('/home/mtk');
-      }
-    })
+    this.platform.backButton.subscribe(() => {
+      navigator["app"].exitApp();
+    });
   }
 }
